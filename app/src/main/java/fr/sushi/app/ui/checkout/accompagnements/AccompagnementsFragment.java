97184859af741515c8ceeba75ce9322336fe9204
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-05-11 13:43:48 +0600

    Commade getting - Done

--- app/src/main/java/fr/sushi/app/ui/checkout/accompagnements/AccompagnementsFragment.java
+++ app/src/main/java/fr/sushi/app/ui/checkout/accompagnements/AccompagnementsFragment.java
@@ -1,6 +1,7 @@
 package fr.sushi.app.ui.checkout.accompagnements;
 
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import fr.sushi.app.R;
+import fr.sushi.app.ui.checkout.commade.CommadeViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccompagnementsFragment extends Fragment {
-
+    private CommadeViewModel commadeViewModel;
 
     public AccompagnementsFragment() {
         // Required empty public constructor
@@ -25,6 +27,16 @@ public class AccompagnementsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_accompagnements, container, false);
+    }
+
+    private void observeData() {
+
+        commadeViewModel = ViewModelProviders.of(this).get(CommadeViewModel.class);
+        commadeViewModel.getCheckoutSideProducts();
+
+        commadeViewModel.getSideProductMutableLiveData().observe(this, responseBody -> {
+
+        });
     }
 
 }
