0c0f2d1fb5645a4787ae2166fb22c050f77dfb22
tariqul <tariqul@w3engineers.com>
2019-05-13 17:04:34 +0600

    Address add, update done. Log out done

---
+++ app/src/main/java/fr/sushi/app/ui/carddetails/CardDetailsActivity.java
@@ -0,0 +1,15 @@
+package fr.sushi.app.ui.carddetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.sushi.app.R;
+
+public class CardDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_details);
+    }
+}
