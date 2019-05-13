3564d3c0a918d792a0be9c27038425d92fb25e65
Shantu Chandra Dash <shantudash@Shantus-MacBook-Pro.local>
2019-05-13 10:39:43 +0600

    count feature done for all six items

--- app/src/main/java/fr/sushi/app/ui/checkout/accompagnements/AccompagnementsFragment.java
+++ app/src/main/java/fr/sushi/app/ui/checkout/accompagnements/AccompagnementsFragment.java
@@ -40,17 +40,20 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
     private AccompagnementViewModel accompagnementViewModel;
     private RecyclerView recycler_view_accompagnements;
     private AccompagnementsAdapter adapter;
-    private RelativeLayout rlSauces,rlAccompagnements,rlBoissons;
-    private RelativeLayout rlCountForSauces,rlCountForAccompagnements,rlCountForBoissons;
-    private TextView tvCountSauces,tvCountAccompagnements,tvCountBoissons;
-    private TextView tvSauces,tvAccompagnements,tvBoissons;
+    private RelativeLayout rlSauces,rlAccompagnements,rlBoissons,rlDesserts,rlWasbi,rlBaguettes;
+    private RelativeLayout rlCountForSauces,rlCountForAccompagnements,rlCountForBoissons,rlCountForDesserts,rlCountForWasbi,rlCountForBaguettes;
+    private TextView tvCountSauces,tvCountAccompagnements,tvCountBoissons,tvCountDesserts,tvCountWasbi,tvCountBaguettes;
+    private TextView tvSauces,tvAccompagnements,tvBoissons,tvDesserts,tvWasbi,tvBaguettes;
     private FragmentAccompagnementsBinding binding;
     
     private int selectedAccompagnements;
     private static final int SAUCES = 1;
     private static final int ACCOMPAGNEMENTS = 2;
     private static final int BOISSONS = 3;
-    private int countSauces = 0, countAccompagnements = 0, countBoissons = 0;
+    private static final int DESSERTS = 4;
+    private static final int WASBI = 5;
+    private static final int BAGUETTES = 6;
+    private int countSauces = 0, countAccompagnements = 0, countBoissons = 0, countDesserts = 0 , countWasbi = 0 ,countBauettes=0 ;
 
 
 
@@ -92,6 +95,24 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
         tvCountBoissons=view.findViewById(R.id.tvCountBoissons);
         tvBoissons=view.findViewById(R.id.tvBoissons);
 
+        rlDesserts=view.findViewById(R.id.rlDesserts);
+        rlDesserts.setOnClickListener(this);
+        rlCountForDesserts=view.findViewById(R.id.rlCountForDesserts);
+        tvCountDesserts=view.findViewById(R.id.tvCountDesserts);
+        tvDesserts=view.findViewById(R.id.tvDesserts);
+
+        rlWasbi=view.findViewById(R.id.rlWasbi);
+        rlWasbi.setOnClickListener(this);
+        rlCountForWasbi=view.findViewById(R.id.rlCountForWasbi);
+        tvCountWasbi=view.findViewById(R.id.tvCountWasbi);
+        tvWasbi=view.findViewById(R.id.tvWasbi);
+
+        rlBaguettes=view.findViewById(R.id.rlBaguettes);
+        rlBaguettes.setOnClickListener(this);
+        rlCountForBaguettes=view.findViewById(R.id.rlCountForBaguettes);
+        tvCountBaguettes=view.findViewById(R.id.tvCountBaguettes);
+        tvBaguettes=view.findViewById(R.id.tvBaguettes);
+
 
         recycler_view_accompagnements = view.findViewById(R.id.recycler_view_accompagnements);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
@@ -130,6 +151,36 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
                            rlCountForBoissons.setVisibility(View.GONE);
                        }
                        break;
+                   case DESSERTS:
+                       Log.e(TAG, "iconImageViewPlusOnClick: DESSERTS clicked");
+                       countDesserts+=1;
+                       tvCountDesserts.setText(String.valueOf(countDesserts));
+                       if (countDesserts>0){
+                           rlCountForDesserts.setVisibility(View.VISIBLE);
+                       }else {
+                           rlCountForDesserts.setVisibility(View.GONE);
+                       }
+                       break;
+                   case WASBI:
+                       Log.e(TAG, "iconImageViewPlusOnClick: WASBI clicked");
+                       countWasbi+=1;
+                       tvCountWasbi.setText(String.valueOf(countWasbi));
+                       if (countWasbi>0){
+                           rlCountForWasbi.setVisibility(View.VISIBLE);
+                       }else {
+                           rlCountForWasbi.setVisibility(View.GONE);
+                       }
+                       break;
+                   case BAGUETTES:
+                       Log.e(TAG, "iconImageViewPlusOnClick: BAGUETTES clicked");
+                       countBauettes +=1;
+                       tvCountBaguettes.setText(String.valueOf(countBauettes));
+                       if (countBauettes>0){
+                           rlCountForBaguettes.setVisibility(View.VISIBLE);
+                       }else {
+                           rlCountForBaguettes.setVisibility(View.GONE);
+                       }
+                       break;
                }
             }
 
@@ -137,7 +188,7 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
             public void iconImageViewMinusOnClick(int position) {
                 switch (selectedAccompagnements){
                     case SAUCES:
-                        Log.e(TAG, "iconImageViewPlusOnClick: SAUCES clicked");
+                        Log.e(TAG, "iconImageViewMinusOnClick: SAUCES clicked");
                         countSauces-=1;
                         tvCountSauces.setText(String.valueOf(countSauces));
                         if (countSauces>0){
@@ -147,7 +198,7 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
                         }
                         break;
                     case ACCOMPAGNEMENTS:
-                        Log.e(TAG, "iconImageViewPlusOnClick: ACCOMPAGNEMENTS clicked");
+                        Log.e(TAG, "iconImageViewMinusOnClick: ACCOMPAGNEMENTS clicked");
                         countAccompagnements-=1;
                         tvCountAccompagnements.setText(String.valueOf(countAccompagnements));
                         if (countAccompagnements>0){
@@ -157,13 +208,43 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
                         }
                         break;
                     case BOISSONS:
-                        Log.e(TAG, "iconImageViewPlusOnClick: BOISSONS clicked");
+                        Log.e(TAG, "iconImageViewMinusOnClick: BOISSONS clicked");
                         countBoissons-=1;
                         tvCountBoissons.setText(String.valueOf(countBoissons));
                         if (countBoissons>0){
                             rlCountForBoissons.setVisibility(View.VISIBLE);
                         }else {
                             rlCountForBoissons.setVisibility(View.GONE);
+                        }
+                        break;
+                    case DESSERTS:
+                        Log.e(TAG, "iconImageViewMinusOnClick: DESSERTS clicked");
+                        countDesserts-=1;
+                        tvCountDesserts.setText(String.valueOf(countDesserts));
+                        if (countDesserts>0){
+                            rlCountForDesserts.setVisibility(View.VISIBLE);
+                        }else {
+                            rlCountForDesserts.setVisibility(View.GONE);
+                        }
+                        break;
+                    case WASBI:
+                        Log.e(TAG, "iconImageViewMinusOnClick: WASBI clicked");
+                        countWasbi-=1;
+                        tvCountWasbi.setText(String.valueOf(countWasbi));
+                        if (countWasbi>0){
+                            rlCountForWasbi.setVisibility(View.VISIBLE);
+                        }else {
+                            rlCountForWasbi.setVisibility(View.GONE);
+                        }
+                        break;
+                    case BAGUETTES:
+                        Log.e(TAG, "iconImageViewMinusOnClick: BAGUETTES clicked");
+                        countBauettes -=1;
+                        tvCountBaguettes.setText(String.valueOf(countBauettes));
+                        if (countBauettes>0){
+                            rlCountForBaguettes.setVisibility(View.VISIBLE);
+                        }else {
+                            rlCountForBaguettes.setVisibility(View.GONE);
                         }
                         break;
                 }
@@ -202,41 +283,143 @@ public class AccompagnementsFragment extends Fragment implements View.OnClickLis
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.rlSauces:
-                selectedAccompagnements=1;
-                tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-                tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-                tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-
-                rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-                rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-                rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-
-                recycler_view_accompagnements.setVisibility(View.VISIBLE);
+                handleSauce();
             break;
             case R.id.rlAccompagnements:
-                selectedAccompagnements=2;
-                tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-                tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-                tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-
-                rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-                rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-                rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-
-                recycler_view_accompagnements.setVisibility(View.VISIBLE);
+                handleAccompanements();
                 break;
             case R.id.rlBoissons:
-                selectedAccompagnements=3;
-                tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-                tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-                tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-
-                rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
-                rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-                rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
-
-                recycler_view_accompagnements.setVisibility(View.VISIBLE);
+                handleBoissons();
                 break;
+            case R.id.rlDesserts:
+                handleDesserts();
+                break;
+            case R.id.rlWasbi:
+               handleWasbi();
+                break;
+            case R.id.rlBaguettes:
+                handleBaguettes();
+                break;
+
         }
+    }
+
+    private void handleBaguettes() {
+        selectedAccompagnements=6;
+
+        tvBaguettes.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        tvWasbi.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvDesserts.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+
+        rlBaguettes.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        rlWasbi.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlDesserts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+
+        recycler_view_accompagnements.setVisibility(View.VISIBLE);
+    }
+
+    private void handleWasbi() {
+        selectedAccompagnements=5;
+
+        tvWasbi.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        tvDesserts.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBaguettes.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+
+        rlWasbi.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        rlDesserts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBaguettes.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+
+        recycler_view_accompagnements.setVisibility(View.VISIBLE);
+    }
+
+    private void handleDesserts() {
+        selectedAccompagnements=4;
+
+        tvDesserts.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvWasbi.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBaguettes.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+
+        rlDesserts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlWasbi.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBaguettes.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+        recycler_view_accompagnements.setVisibility(View.VISIBLE);
+    }
+
+    private void handleBoissons() {
+        selectedAccompagnements=3;
+        tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvDesserts.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvWasbi.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBaguettes.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+
+        rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlDesserts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlWasbi.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBaguettes.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+        recycler_view_accompagnements.setVisibility(View.VISIBLE);
+    }
+
+    private void handleAccompanements() {
+        selectedAccompagnements=2;
+        tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvDesserts.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvWasbi.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBaguettes.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+
+        rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlDesserts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlWasbi.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBaguettes.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+        recycler_view_accompagnements.setVisibility(View.VISIBLE);
+    }
+
+    private void handleSauce() {
+        selectedAccompagnements=1;
+        tvSauces.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        tvAccompagnements.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBoissons.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvDesserts.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvWasbi.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        tvBaguettes.setTextColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+
+        rlSauces.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_627588));
+        rlAccompagnements.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBoissons.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlDesserts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlWasbi.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        rlBaguettes.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+
+        recycler_view_accompagnements.setVisibility(View.VISIBLE);
     }
 }
