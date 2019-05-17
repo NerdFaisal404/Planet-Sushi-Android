c40adb2b92fc3fd7aa289b3b3c2bad8e7bd43282
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-05-17 15:30:47 +0600

    food menu bottom address showing - Done

--- app/src/main/java/fr/sushi/app/ui/cart/FoodMenuFragment.java
+++ app/src/main/java/fr/sushi/app/ui/cart/FoodMenuFragment.java
@@ -8,15 +8,21 @@ import android.view.View;
 import com.example.library.FocusResizeScrollListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import fr.sushi.app.R;
+import fr.sushi.app.data.local.SharedPref;
 import fr.sushi.app.data.local.helper.CommonUtility;
+import fr.sushi.app.data.local.preference.PrefKey;
 import fr.sushi.app.data.model.food_menu.CategoriesItem;
 import fr.sushi.app.databinding.FragmentCartBinding;
 import fr.sushi.app.ui.base.BaseFragment;
 import fr.sushi.app.ui.cart.adapter.FoodMenuAdapterFocus;
 import fr.sushi.app.ui.cart.viewmodel.FoodMenuViewModel;
+import fr.sushi.app.ui.home.PlaceUtil;
+import fr.sushi.app.ui.home.SearchPlace;
 import fr.sushi.app.ui.menu.MenuDetailsActivity;
 import fr.sushi.app.util.DataCacheUtil;
 
@@ -39,6 +45,30 @@ public class FoodMenuFragment extends BaseFragment implements FoodMenuAdapterFoc
     protected void startUI() {
         binding = (FragmentCartBinding) getViewDataBinding();
         observeData();
+
+        Date currentTime = Calendar.getInstance().getTime();
+
+        boolean isLivarsion = SharedPref.readBoolean(PrefKey.IS_LIBRATION_PRESSED, false);
+        boolean isExporter = SharedPref.readBoolean(PrefKey.IS_EMPORTER_PRESSED, false);
+        if (isLivarsion) {
+            binding.tvDeliveryType.setText("Livraison");
+            binding.tvDeliveryInfo.setText("prévue pour " + currentTime);
+        } else if (isExporter) {
+            binding.tvDeliveryType.setText("A emporter");
+            binding.tvDeliveryInfo.setText("prévue pour " + currentTime);
+        } else {
+            binding.tvDeliveryType.setText("Livraison");
+            binding.tvDeliveryInfo.setText("prévue pour " + currentTime);
+        }
+
+        List<SearchPlace> recentSearchPlace = PlaceUtil.getSearchPlace();
+        if (!recentSearchPlace.isEmpty()) {
+            SearchPlace place = recentSearchPlace.get(0);
+            binding.tvLocationInfo.setText(place.getAddress() + "-" + place.getCity() + ", " + place.getPostalCode());
+            binding.tvDeliveryInfo.setText("prévue pour " + place.getTime());
+
+        }
+
 
     }
 
@@ -70,8 +100,8 @@ public class FoodMenuFragment extends BaseFragment implements FoodMenuAdapterFoc
     void setAdapter() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         binding.featuredRecyclerView.setLayoutManager(linearLayoutManager);
-        foodMenuAdapter = new FoodMenuAdapterFocus(getActivity(), this,(int) getResources().getDimension(R.dimen.custom_item_height));
-      //  binding.featuredRecyclerView.setHasFixedSize(true);
+        foodMenuAdapter = new FoodMenuAdapterFocus(getActivity(), this, (int) getResources().getDimension(R.dimen.custom_item_height));
+        //  binding.featuredRecyclerView.setHasFixedSize(true);
         binding.featuredRecyclerView.setAdapter(foodMenuAdapter);
         binding.featuredRecyclerView.addOnScrollListener(new FocusResizeScrollListener<>(foodMenuAdapter, linearLayoutManager));
     }
