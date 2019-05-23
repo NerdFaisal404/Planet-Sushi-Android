07e06f48a6dee57a3eb0d1d8a739c96c72942342
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-05-23 13:28:53 +0600

    payment success page design inprogress

---
+++ app/src/main/java/fr/sushi/app/ui/checkout/PaymentSuccssActivity.java
@@ -0,0 +1,15 @@
+package fr.sushi.app.ui.checkout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.sushi.app.R;
+
+public class PaymentSuccssActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_succss);
+    }
+}
