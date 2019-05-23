2550e16ba40f718486337734deaf54506ddd3abf
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-05-23 13:56:34 +0600

    payment success page design inprogress

--- app/src/main/java/fr/sushi/app/ui/profile/ProfileFragment.java
+++ app/src/main/java/fr/sushi/app/ui/profile/ProfileFragment.java
@@ -3,14 +3,8 @@ package fr.sushi.app.ui.profile;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 
 import java.util.ArrayList;
